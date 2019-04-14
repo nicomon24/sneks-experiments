17234d31a20acb3bfc65f2f11c4877d9123db627
nicomon24 <nico.montali24@gmail.com>
2019-04-14 23:28:00 +0200

    Removed unnecessary wrappers

--- common/atari_wrappers.py
+++ common/atari_wrappers.py
@@ -6,7 +6,6 @@ import gym
 from gym import spaces
 import cv2
 cv2.ocl.setUseOpenCL(False)
-from .wrappers import TimeLimit
 
 
 class NoopResetEnv(gym.Wrapper):
@@ -218,28 +217,3 @@ class LazyFrames(object):
 
     def __getitem__(self, i):
         return self._force()[i]
-
-def make_atari(env_id, max_episode_steps=None):
-    env = gym.make(env_id)
-    assert 'NoFrameskip' in env.spec.id
-    env = NoopResetEnv(env, noop_max=30)
-    env = MaxAndSkipEnv(env, skip=4)
-    if max_episode_steps is not None:
-        env = TimeLimit(env, max_episode_steps=max_episode_steps)
-    return env
-
-def wrap_deepmind(env, episode_life=True, clip_rewards=True, frame_stack=False, scale=False):
-    """Configure environment for DeepMind-style Atari.
-    """
-    if episode_life:
-        env = EpisodicLifeEnv(env)
-    if 'FIRE' in env.unwrapped.get_action_meanings():
-        env = FireResetEnv(env)
-    env = WarpFrame(env)
-    if scale:
-        env = ScaledFloatFrame(env)
-    if clip_rewards:
-        env = ClipRewardEnv(env)
-    if frame_stack:
-        env = FrameStack(env, 4)
-    return env
