29820a1cf80da910abef074526debe14b4d4ff9b
nicomon24 <nico.montali24@gmail.com>
2019-04-15 22:54:51 +0200

    Changed network

--- dqn/qnetwork.py
+++ dqn/qnetwork.py
@@ -16,7 +16,7 @@ def conv2d_size_out(size, kernel_size = 1, stride = 1):
 
 class QNetwork(nn.Module):
 
-    def __init__(self, observation_space, action_space, layers=[(16, 8, 1), (32, 4, 1)]):
+    def __init__(self, observation_space, action_space, layers=[(16, 8, 4), (64, 4, 2), (64, 3, 1)]):
         super().__init__()
         # Check that observation space is box
         assert isinstance(observation_space, gym.spaces.Box), "Only works for box environments."
@@ -44,9 +44,9 @@ class QNetwork(nn.Module):
         self.conv_layers = nn.Sequential(*self.conv_layers)
 
         # Declare the last linear layer, input size is the last conv layer flattened
-        self.head = nn.Sequential(nn.Linear(size0 * size0 * channels, 256),
+        self.head = nn.Sequential(nn.Linear(size0 * size0 * channels, 512),
                                   nn.ReLU(),
-                                  nn.Linear(256, self.output_shape))
+                                  nn.Linear(512, self.output_shape))
 
     def forward(self, batch):
         # Pass the batch through the conv layers
