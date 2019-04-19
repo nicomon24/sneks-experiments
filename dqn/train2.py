9f1eeaf557ea593dd106b44992d2cc028b5b745c
nicomon24 <nico.montali24@gmail.com>
2019-04-19 21:58:54 +0200

    Added loss logging

--- dqn/train2.py
+++ dqn/train2.py
@@ -54,7 +54,7 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             batch = buffer.sample(params['batch_size'])
             loss_v = common.calc_loss_dqn(batch, net, tgt_net.target_model, gamma=params['gamma'], cuda=args.cuda)
-            writer.add_scalar('loss', loss, frame_idx)
+            writer.add_scalar('loss', loss_v, frame_idx)
             loss_v.backward()
             optimizer.step()
 
