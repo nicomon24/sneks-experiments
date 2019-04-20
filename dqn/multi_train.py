8109a3682ab3f801ad7d9509c7bf65a6612cca92
nicomon24 <nico.montali24@gmail.com>
2019-04-20 21:34:54 +0200

    Added async training for dqn

--- dqn/multi_train.py
+++ dqn/multi_train.py
@@ -4,14 +4,17 @@ import ptan
 import argparse
 import time
 import numpy as np
+import sneks
 
 import torch
 from torch import nn, optim
 import torch.nn.functional as F
-
-from qnetwork import QNetwork
-
+import torch.multiprocessing as mp
 from tensorboardX import SummaryWriter
+
+from common.atari_wrappers import EpisodicLifeEnv, NoopResetEnv, MaxAndSkipEnv, FireResetEnv, WarpFrame, FrameStack, ClipRewardEnv, ScaledFloatFrame
+from common.pytorch_utils import ImageToPyTorch
+from qnetwork import QNetwork
 
 EPSILON_START = 1.0
 EPSILON_STOP = 0.02
@@ -19,7 +22,16 @@ PLAY_STEPS = 2
 
 def make_env(env_name, rnd_seed):
     env = gym.make(env_name)
-    env = ptan.common.wrappers.wrap_dqn(env)
+    #env = EpisodicLifeEnv(env)
+    #env = NoopResetEnv(env)
+    #env = MaxAndSkipEnv(env)
+    #env = FireResetEnv(env)
+    #env = WarpFrame(env)
+    #env = FrameStack(env, 4)
+    #env = ClipRewardEnv(env)
+    env = ScaledFloatFrame(env)
+    env = ImageToPyTorch(env)
+    env.seed(rnd_seed)
     return env
 
 def unpack_batch(batch):
@@ -37,7 +49,7 @@ def unpack_batch(batch):
     return np.array(states, copy=False), np.array(actions), np.array(rewards, dtype=np.float32), \
            np.array(dones, dtype=np.uint8), np.array(last_states, copy=False)
 
-def play_func(env_name, net, exp_queue, seed=42):
+def play_func(env_name, net, exp_queue, seed=42, timesteps=1, epsilon_decay_last_step=1000, gamma=1.0):
     # Create the environment
     env = make_env(env_name, seed)
     # Get PyTorch device
@@ -48,45 +60,65 @@ def play_func(env_name, net, exp_queue, seed=42):
     exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=gamma, steps_count=1)
     exp_source_iter = iter(exp_source)
 
-    .....
-
-
-
-def train(env_name, seed=42, timesteps=1, epsilon_decay_last_step=1000,
-            er_capacity=1e4, batch_size=16, lr=1e-3, gamma=1.0,  update_target=16,
-            logdir='logs', init_timesteps=100):
-
-    # Create tensorboard writer
-    writer = SummaryWriter(logdir)
-
-    # Create the Q network
-    net = QNetwork(env.observation_space, env.action_space).to(device)
-    tgt_net = ptan.agent.TargetNet(net)
-
-
-
-
-    buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=er_capacity)
-    optimizer = optim.Adam(net.parameters(), lr=lr)
-
-    ep_start_step, ep_start_time = 0, time.time()
-
-    for timestep in range(0, timesteps, PLAY_STEPS):
-
+    timestep, ep_start_step, ep_start_time = 0, 0, time.time()
+    while timestep < timesteps:
         # Epsilon starts from EPSILON_START and linearly decreases till epsilon_decay_last_step to EPSILON_STOP
         epsilon = EPSILON_STOP + max(0, (EPSILON_START - EPSILON_STOP)*(epsilon_decay_last_step-timestep)/epsilon_decay_last_step)
+        #writer.add_scalar('internals/epsilon', epsilon, timestep)
         selector.epsilon = epsilon
-        # Add one step to the buffer
-        buffer.populate(PLAY_STEPS)
+        # Do one step
+        timestep += 1
+        exp = next(exp_source_iter)
+
         new_rewards = exp_source.pop_total_rewards()
+        ep_info = None
         if new_rewards:
             ep_len = timestep - ep_start_step
             speed = (ep_len) / (time.time() - ep_start_time)
             ep_start_step, ep_start_time = timestep, time.time()
-            # Write performance
-            writer.add_scalar('performance/return', new_rewards[0], timestep)
-            writer.add_scalar('performance/length', ep_len, timestep)
-            writer.add_scalar('performance/speed', speed, timestep)
+            ep_info = (new_rewards[0], ep_len, speed)
+        exp_queue.put((exp, ep_info))
+
+    exp_queue.put((None, None))
+
+def train(env_name, seed=42, timesteps=1, epsilon_decay_last_step=1000,
+            er_capacity=1e4, batch_size=16, lr=1e-3, gamma=1.0,  update_target=16,
+            logdir='logs', init_timesteps=100, save_every_steps=1e5):
+    # Multiprocessing method
+    mp.set_start_method('spawn')
+
+    # Get PyTorch device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # Create tensorboard writer
+    writer = SummaryWriter(logdir)
+
+    # Create the Q network
+    _env = make_env(env_name, seed)
+    net = QNetwork(_env.observation_space, _env.action_space).to(device)
+    tgt_net = ptan.agent.TargetNet(net)
+    # Create buffer and optimizer
+    buffer = ptan.experience.ExperienceReplayBuffer(experience_source=None, buffer_size=er_capacity)
+    optimizer = optim.Adam(net.parameters(), lr=lr)
+
+    # Multiprocessing queue
+    exp_queue = mp.Queue(maxsize=PLAY_STEPS * 2)
+    play_proc = mp.Process(target=play_func, args=(env_name, net, exp_queue, seed, timesteps, epsilon_decay_last_step, gamma))
+    play_proc.start()
+
+    timestep = 0
+    while play_proc.is_alive() and timestep < timesteps:
+        timestep += PLAY_STEPS
+
+        for _ in range(PLAY_STEPS):
+            exp, ep_info = exp_queue.get()
+            if exp is None:
+                play_proc.join()
+                break
+            buffer._add(exp)
+            if ep_info is not None:
+                writer.add_scalar('performance/return', ep_info[0], timestep)
+                writer.add_scalar('performance/length', ep_info[1], timestep)
+                writer.add_scalar('performance/speed', ep_info[2], timestep)
 
         if len(buffer) < init_timesteps:
             continue
@@ -107,7 +139,6 @@ def train(env_name, seed=42, timesteps=1, epsilon_decay_last_step=1000,
         loss = F.mse_loss(state_action_values, expected_state_action_values)
 
         writer.add_scalar('internals/loss', loss.item(), timestep)
-        writer.add_scalar('internals/epsilon', epsilon, timestep)
         loss.backward()
         for param in net.parameters():
             param.grad.data.clamp_(-1, 1)
@@ -115,6 +146,9 @@ def train(env_name, seed=42, timesteps=1, epsilon_decay_last_step=1000,
 
         if timestep % update_target == 0:
             tgt_net.sync()
+
+        if timestep % save_every_steps == 0:
+            torch.save(net.state_dict(), 'qnetwork-multi.pth')
 
 if __name__ == '__main__':
     # Check also for scientific notation
---
+++ dqn/play.py
@@ -0,0 +1,72 @@
+'''
+    Play episodes using the trained policy.
+'''
+#!/usr/bin/env python3
+import gym
+import ptan
+import argparse
+import time
+import numpy as np
+import sneks
+
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+from qnetwork import QNetwork
+from tensorboardX import SummaryWriter
+from common.atari_wrappers import EpisodicLifeEnv, NoopResetEnv, MaxAndSkipEnv, FireResetEnv, WarpFrame, FrameStack, ClipRewardEnv, ScaledFloatFrame
+from common.pytorch_utils import ImageToPyTorch
+
+EPSILON = 0.02
+
+def make_env(env_name, rnd_seed):
+    env = gym.make(env_name)
+    #env = EpisodicLifeEnv(env)
+    #env = NoopResetEnv(env)
+    #env = MaxAndSkipEnv(env)
+    #env = FireResetEnv(env)
+    #env = WarpFrame(env)
+    #env = FrameStack(env, 4)
+    #env = ClipRewardEnv(env)
+    env = ScaledFloatFrame(env)
+    env = ImageToPyTorch(env)
+    env.seed(rnd_seed)
+    return env
+
+def play(env_name, seed=42, model=None):
+    # Create the environment
+    env = make_env(env_name, seed)
+    # Get PyTorch device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Create Qnetwork
+    net = QNetwork(env.observation_space, env.action_space).to(device)
+    net.load_state_dict(torch.load(model, map_location="cuda" if torch.cuda.is_available() else "cpu"))
+
+    obs, ep_return, ep_len = env.reset(), 0, 0
+    while True:
+
+        action = net(torch.from_numpy(np.expand_dims(obs, 0)).to(device)).argmax(dim=1)[0]
+        obs, reward, done , _ = env.step(action)
+        env.render()
+        ep_return += reward
+        ep_len += 1
+
+        if done:
+            print("Episode:", ep_return, '\t\t', ep_len)
+            obs, ep_return, ep_len = env.reset(), 0, 0
+
+        time.sleep(0.1)
+
+if __name__ == '__main__':
+    # Check also for scientific notation
+    def int_scientific(x):
+        return int(float(x))
+    # Declare and parse command line arguments
+    import argparse
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--env_name', help='Environment to train on.', type=str, default='snek-rgb-16-v1')
+    parser.add_argument('--model', help='Path of the model to load.', type=str, default=None)
+    parser.add_argument('--seed', help='Random seed.', type=int, default=42)
+    args = parser.parse_args()
+    # Call the train function with arguments
+    play(**vars(args))
