d1d56fef3f02de1fbf7f1cf31588a03f24ba8de3
nicomon24 <nico.montali24@gmail.com>
2019-04-20 03:07:17 +0200

    Added snek support for DQN

--- common/pytorch_utils.py
+++ common/pytorch_utils.py
@@ -3,6 +3,7 @@
 """
 
 import numpy as np
+import gym
 
 def NCHW_from_NHWC(x):
     x = np.array(x)
@@ -21,3 +22,16 @@ def NHWC_from_NCHW(x):
         return np.transpose(x, (1, 2, 0))
     else:
         raise Exception("Unrecognized shape.")
+
+class ImageToPyTorch(gym.ObservationWrapper):
+    """
+    Change image shape to CWH
+    """
+    def __init__(self, env):
+        super(ImageToPyTorch, self).__init__(env)
+        old_shape = self.observation_space.shape
+        self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=(old_shape[-1], old_shape[0], old_shape[1]),
+                                                dtype=np.float32)
+
+    def observation(self, observation):
+        return np.swapaxes(observation, 2, 0)
