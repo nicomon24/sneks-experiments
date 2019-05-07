b1329bd1b00e573a290b5014077b343a7b541ed2
nicomon24 <nico.montali24@gmail.com>
2019-05-07 16:21:16 +0200

    Added sacred without observer

--- dqn/train_sacred.py
+++ dqn/train_sacred.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # ======== IMPORTS ========
 # General import
-import argparse, time
+import argparse, time, os
 import gym, sneks
 import ptan
 import numpy as np
@@ -13,7 +13,7 @@ import torch.multiprocessing as mp
 from torch.optim.lr_scheduler import StepLR
 from tensorboardX import SummaryWriter
 # Local imports
-from common.atari_wrappers import EpisodicLifeEnv, NoopResetEnv, MaxAndSkipEnv, FireResetEnv, WarpFrame, FrameStack, ClipRewardEnv, ScaledFloatFrame
+from common.atari_wrappers import ScaledFloatFrame
 from common.pytorch_utils import ImageToPyTorch
 from common.logger import Logger
 from qnetwork import QNetwork
@@ -21,21 +21,32 @@ from qnetwork import QNetwork
 from sacred import Experiment
 
 # ======== SACRED CONFIG ========
+if os.environ.get('EXPERIMENT_NAME') is not None:
+    ex = Experiment(os.environ.get('EXPERIMENT_NAME'))
+else:
+    ex = Experiment('DQN')
+
 @ex.config
-def custom_config():
+def base_config():
+    experiment_name = os.environ.get('EXPERIMENT_NAME', 'test')    # Experiment name
     env_name = 'snek-rgb-16-v1'         # Environment to play
     arch = 'nature'                     # Architecture for the network
-    exp_name = 'test'                   # Experiment name
     timesteps = 1e6                     # Training timesteps
     init_timesteps = 1e4                # Timestep to bootstrap
     seed = 42                           # Random seed
-
-
-
-EPSILON_START = 1.0
-EPSILON_STOP = 0.05
-PLAY_STEPS = 2
-LR_STEPS = 1000
+    er_capacity = 1e5                   # Capacity of †he experience replay
+    epsilon_start = 1.0                 # Starting epsilon
+    epsilon_stop = 0.05                 # Final epsilon after decay
+    epsilon_decay_stop = 1e5            # Timestep at which we end decay
+    batch_size = 16                     # Batch size
+    target_sync = 1e3                   # How frequently we sync target net
+    lr = 1e-3                           # Starting learning rate
+    gamma = 0.99                        # Discount factor
+    dueling = False                     # Use dueling network
+    play_steps = 2                      # How many play steps per iteration
+    lr_steps = 1e4                      # How frequently we decay LR
+    lr_gamma = 0.99                     # Decay factor for LR
+    save_steps = 5e4                    # How frequently we save a checkpoint
 
 def make_env(env_name, rnd_seed):
     """
@@ -70,7 +81,7 @@ def unpack_batch(batch):
     return np.array(states, copy=False), np.array(actions), np.array(rewards, dtype=np.float32), \
            np.array(dones, dtype=np.uint8), np.array(last_states, copy=False)
 
-def play_func(env_name, net, exp_queue, seed=42, timesteps=1, epsilon_decay_last_step=1000, gamma=1.0):
+def play_func(env_name, net, exp_queue, seed=42, timesteps=1, epsilon_schedule=(1.0, 0.05, 1000), gamma=1.0):
     """
         Function called to play episodes in parallel.
     """
@@ -84,12 +95,14 @@ def play_func(env_name, net, exp_queue, seed=42, timesteps=1, epsilon_decay_last
     # Create experience source, i.e. the wrapped environment.
     exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=gamma, steps_count=1)
     exp_source_iter = iter(exp_source)
+    # Parse epsilon config
+    epsilon_start, epsilon_stop, epsilon_decay_stop = epsilon_schedule
 
     # Start the playing loop
     timestep, ep_start_step, ep_start_time = 0, 0, time.time()
     while timestep < timesteps:
         # Epsilon starts from EPSILON_START and linearly decreases till epsilon_decay_last_step to EPSILON_STOP
-        epsilon = EPSILON_STOP + max(0, (EPSILON_START - EPSILON_STOP)*(epsilon_decay_last_step-timestep)/epsilon_decay_last_step)
+        epsilon = epsilon_stop + max(0, (epsilon_start - epsilon_stop)*(epsilon_decay_stop-timestep)/epsilon_decay_stop)
         selector.epsilon = epsilon
         # Do one step
         timestep += 1
@@ -108,10 +121,11 @@ def play_func(env_name, net, exp_queue, seed=42, timesteps=1, epsilon_decay_last
     # End
     exp_queue.put((None, {}))
 
-def train(env_name, seed=42, timesteps=1, epsilon_decay_last_step=1000,
-            er_capacity=1e4, batch_size=16, lr=1e-3, gamma=1.0,  update_target=16,
-            exp_name='test', init_timesteps=100, save_every_steps=1e4, arch='nature',
-            dueling=False):
+@ex.capture
+def train(env_name, arch, timesteps=1, init_timesteps=0, seed=42, er_capacity=1,
+            epsilon_start=1.0, epsilon_stop=0.05, epsilon_decay_stop=1, batch_size=16,
+            target_sync=16, lr=1e-3, gamma=1.0, dueling=False, play_steps=1, lr_steps=1e4,
+            lr_gamma=0.99, save_steps=5e4, logger=None, experiment_name='test'):
     """
         Main training function. Calls the subprocesses to get experience and
         train the network.
@@ -121,8 +135,6 @@ def train(env_name, seed=42, timesteps=1, epsilon_decay_last_step=1000,
 
     # Get PyTorch device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # Create logger
-    logger = Logger(exp_name, loggers=['tensorboard'])
 
     # Create the Q network
     _env = make_env(env_name, seed)
@@ -132,20 +144,20 @@ def train(env_name, seed=42, timesteps=1, epsilon_decay_last_step=1000,
     # Create buffer and optimizer
     buffer = ptan.experience.ExperienceReplayBuffer(experience_source=None, buffer_size=er_capacity)
     optimizer = optim.Adam(net.parameters(), lr=lr)
-    scheduler = StepLR(optimizer, step_size=LR_STEPS, gamma=0.99)
+    scheduler = StepLR(optimizer, step_size=lr_steps, gamma=0.99)
 
     # Multiprocessing queue
-    exp_queue = mp.Queue(maxsize=PLAY_STEPS * 2)
-    play_proc = mp.Process(target=play_func, args=(env_name, net, exp_queue, seed, timesteps, epsilon_decay_last_step, gamma))
+    epsilon_schedule = (epsilon_start, epsilon_stop, epsilon_decay_stop)
+    exp_queue = mp.Queue(maxsize=play_steps * 2)
+    play_proc = mp.Process(target=play_func, args=(env_name, net, exp_queue, seed, timesteps, epsilon_schedule, gamma))
     play_proc.start()
 
     # Main training loop
     timestep = 0
     while play_proc.is_alive() and timestep < timesteps:
-        timestep += PLAY_STEPS
-
+        timestep += play_steps
         # Query the environments and log results if the episode has ended
-        for _ in range(PLAY_STEPS):
+        for _ in range(play_steps):
             exp, info = exp_queue.get()
             if exp is None:
                 play_proc.join()
@@ -165,7 +177,7 @@ def train(env_name, seed=42, timesteps=1, epsilon_decay_last_step=1000,
         logger.log_kv('internals/lr', scheduler.get_lr()[0], timestep)
         # Get a batch from experience replay
         optimizer.zero_grad()
-        batch = buffer.sample(batch_size * PLAY_STEPS)
+        batch = buffer.sample(batch_size * play_steps)
         # Unpack the batch
         states, actions, rewards, dones, next_states = unpack_batch(batch)
         states_v = torch.tensor(states).to(device)
@@ -191,25 +203,12 @@ def train(env_name, seed=42, timesteps=1, epsilon_decay_last_step=1000,
             tgt_net.sync()
 
         # Check if we need to save a checkpoint
-        if timestep % save_every_steps == 0:
-            torch.save(net.get_extended_state(), exp_name + '.pth')
+        if timestep % save_steps == 0:
+            torch.save(net.get_extended_state(), experiment_name + '.pth')
 
-if __name__ == '__main__':
-    # Check also for scientific notation
-    def int_scientific(x):
-        return int(float(x))
-    # Declare and parse command line arguments
-    import argparse
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--init_timesteps', help='Number of initial steps to fill the buffer.', type=int_scientific, default=1e4)
-    parser.add_argument('--seed', help='Random seed.', type=int, default=42)
-    parser.add_argument('--er_capacity', help='Experience replay capacity.', type=int_scientific, default=1e4)
-    parser.add_argument('--epsilon_decay_last_step', help='Step at which the epsilon plateau is reached.', type=int_scientific, default=1e4)
-    parser.add_argument('--batch_size', help='Experience batch size.', type=int, default=16)
-    parser.add_argument('--update_target', help='Number of iterations for each target update.', type=int, default=16)
-    parser.add_argument('--lr', help='Optimizer learning rate.', type=float, default=1e-3)
-    parser.add_argument('--gamma', help='Discount factor for the MDP.', type=float, default=1.0)
-    parser.add_argument("--dueling", default=False, action="store_true", help="Dueling network.")
-    args = parser.parse_args()
-    # Call the train function with arguments
-    train(**vars(args))
+@ex.automain
+def main(experiment_name, _run):
+    # Create logger
+    logger = Logger(experiment_name, loggers=['tensorboard', 'sacred'], sacred_run=_run)
+    # Call train
+    train(logger=logger, experiment_name=experiment_name)
